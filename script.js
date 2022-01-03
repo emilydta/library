1449dc9dde4be2d62ed3fb7bc43da0aa7f1c19d1
Emily <emilydta@users.noreply.github.com>
2022-01-03 12:22:06 +1100

    add basic book info display and functionality

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
+const bookDisplay = document.querySelector(".book-display");
+const addBookButton = document.getElementById("add-book-button");
+const newBookForm = document.getElementById("new-book-form");
+
+let myLibrary = [];
+
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
-    this.info = function() {
-        return `${title} ${author} ${pages} ${readStatus}`;
+}
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+const book2 = new Book('Blah', 'Bdsf', '222 pages', 'not read yet');
+
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+
+function books() {
+    for (i = 0; i < myLibrary.length; i++) { 
+        bookInfo = document.createElement('div');
+        bookDisplay.appendChild(bookInfo);
+        bookInfo.setAttribute("class", "book-information");
+        bookInfo.innerText = `${myLibrary[i].title} \n by ${myLibrary[i].author} \n ${myLibrary[i].pages} \n ${myLibrary[i].readStatus}`; 
     }
 }
 
-const book1 = new Book('The Hobbit', 'by J.R.R. Tolkien', '295 pages', 'not read yet');
-console.log(book1.info());
+books();
+
+function addBookButtonFunction() {
+    
+}
+
+addBookButton.addEventListener("click", () => addBookButtonFunction)
